084ff117fde2c83c9ae324b99a22a08b1306d80e
59naga <i59naga@icloud.com>
2016-06-06 20:41:00 +0900

    chore: add favicon.ico

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const favicon = require('serve-favicon');
 const cors = require('cors');
 const compression = require('compression');
 const narou = require('narou-middleware');
@@ -7,6 +8,7 @@ const port = process.env.PORT || 59798;
 const app = express();
 
 app.set('json spaces', 2);
+app.use(favicon('favicon.ico'))
 app.use(cors());
 app.use(compression());
 app.use(narou({ r18: true }));
